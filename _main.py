63a43f58c0790ec799b33749a03a4442d34b055d
Tommaso Tarantola <109342764+tommasotarantola@users.noreply.github.com>
2024-01-24 17:42:02 +0100

    Add files via upload
    upd

--- _main.py
+++ _main.py
@@ -1,2 +1,4 @@
 import file_manager
 file_manager.cst_multirename_manager()
+file_manager.cst_get_folder_size("C:\\")
+file_manager.cst_get_subfolder_size("C:\\Users\\tommasotarantola\\AppData\\Local")
